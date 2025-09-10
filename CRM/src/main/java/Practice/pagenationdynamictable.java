be3f11693f33c17df417587c215510ec65d34ec9
Jayshree-90 <nitamitali60@gmail.com>
2025-09-10 18:04:51 +0530

    page

--- CRM/src/main/java/Practice/pagenationdynamictable.java
+++ CRM/src/main/java/Practice/pagenationdynamictable.java
@@ -1,10 +1,36 @@
 package Practice;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class pagenationdynamictable {
 
 	public static void main(String[] args) {
 		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://49.249.28.218:8098/");
 		
+	   driver.findElement(By.id("username")).sendKeys("rmgyantra");;
+	   
+	   driver.findElement(By.id("inputPassword")).sendKeys("rmgy@9999");
+	 
+	   driver.findElement(By.xpath("//button[text()='Sign In']")).click();
+	   
+	   ////table[@class='table table-striped table-hover']
+	   WebElement forward = driver.findElement(By.xpath("//a[text()='⟩']"));	
+	   int count=0;
+	   while(driver.findElement(By.xpath("//a[text()='⟩']")).isEnabled()) {
+		   driver.findElement(By.xpath("//a[text()='⟩']")).click();
+		   count++;
+	   }
+	   System.out.println(count);
 
 	}
 
