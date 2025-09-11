7477271c34ae597db804a24fdc4561506e01db09
Jayshree-90 <nitamitali60@gmail.com>
2025-09-11 17:08:34 +0530

    exception

--- CRM/src/main/java/genericUtility/PropertyFileUtility.java
+++ CRM/src/main/java/genericUtility/PropertyFileUtility.java
@@ -4,15 +4,23 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import exception.ElementNotFoundException;
+
 public class PropertyFileUtility {
 	
 	//Read data from properties
 	public String toReadDataFromProperties(String key) throws IOException {
-		FileInputStream fis = new FileInputStream("./src/main/resources/Browser.properties");
-		Properties prop = new Properties();
-		prop.load(fis);
-		String data = prop.getProperty(key);
-		return data;
+		
+		
+		 try {
+			 FileInputStream fis = new FileInputStream("./src/main/resources/Browser.properties");
+				Properties prop = new Properties();
+				prop.load(fis);
+				String data = prop.getProperty(key);
+				return data;
+	        } catch (Exception e) {
+	            throw new ElementNotFoundException("Login page elements not found: " + e.getMessage());
+	        }
 	}
 
 }
