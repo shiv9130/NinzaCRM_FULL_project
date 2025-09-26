610c2db84bf9a084b6b7ba31ab3053e47885e7b2
Jayshree-90 <nitamitali60@gmail.com>
2025-09-26 17:23:12 +0530

    updated

--- CRM/src/test/java/testScripts/exactDataFromPaginationTable.java
+++ CRM/src/test/java/testScripts/exactDataFromPaginationTable.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.poi.EncryptedDocumentException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import BaseClassNinzaCrmFull.baseClass;
@@ -25,25 +24,38 @@ public class exactDataFromPaginationTable extends baseClass {
 		List<WebElement> rows = cp.getCampaignTableRow();
 		List<WebElement> colms = cp.getCampaignTableColumn();
 		// WebElement next = cp.getBtnNext();
-		for (int i = 2; i <= 10; i++) {
+		for (int i = 2; i <= 5; i++) {
 			WebElement ele = cp.getPaginationButton(driver, i);
 
 			wu.waitForElementToBeClickable(driver, ele);
 			ele.click();
 		}
-		List<WebElement> data = driver.findElements(By.xpath("//tr[9]/td[1] | //tr[9]/td[2]"));
+		List<WebElement> heading = driver.findElements(By.xpath("//th[5] | //th[6]"));
+		List<WebElement> data = driver.findElements(By.xpath("//tr[3]/td[5] | //tr[3]/td[6]"));
 
 		int excelCol = 0; // start writing in column 0
 		int excelRow = 0; // or use pageRow if looping pages
+		for(WebElement h : heading) {
+			String txt1 = h.getText();
+			System.out.println(txt1); // debug
+
+			// write into Excel row 0, col0 and col1
+			eu.writeDataIntoExcelFile("pagination", excelRow, excelCol, txt1);
+
+			excelCol++; // move to next column
+
+		}
+		
+		int data_row =1,data_col=0;
 
 		for (WebElement s : data) {
 			String txt = s.getText();
 			System.out.println(txt); // debug
 
 			// write into Excel row 0, col0 and col1
-			eu.writeDataIntoExcelFile("pagination", excelRow, excelCol, txt);
+			eu.writeDataIntoExcelFile("pagination", data_row, data_col, txt);
 
-			excelCol++; // move to next column
+			data_col++; // move to next column
 		}
 
 	}
